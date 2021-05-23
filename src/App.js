964e9ff6b3d2dae39d599d31f8d71953ffc88372
NourTebourski <79874265+NourTebourski@users.noreply.github.com>
2021-05-23 12:06:07 +0100

    del

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
 import React from "react";
 import "./App.css";
-import Navbar from "./components/Navbar";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return (
-    <>
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route path="/" exact />
-        </Switch>
-      </Router>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
